fcd337fc08dac1390b08184cd666ce106e0bab42
Jangchanhi <perarduaadastra0620@naver.com>
2023-07-31 19:35:30 +0900

    commit

--- SKT_Opencv/chap04/11.event_draw.py
+++ SKT_Opencv/chap04/11.event_draw.py
@@ -5,14 +5,22 @@ def onMouse(event, x, y, flags, param):
     global title, pt                            # 전역 변수 참조
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        if pt[0] < 0:
-            pt = (x, y)                         # 시작 좌표 지정
+        if pt[0] < 0: pt = (x, y)                         # 시작 좌표 지정
         else:
+            cv2.rectangle(image, pt, (x,y), (255, 0, 0),2)
+            cv2.imshow(title, image)
+            pt = (-1, -1)
+
             
             
     elif event == cv2.EVENT_RBUTTONDOWN:
         if pt[0] < 0: pt = (x, y)
         else:
+            dx, dy = pt[0] - x, pt[1]-y
+            radius = int(np.squrt(dx*dx + dy* dy))
+            cv2.circle(image, pt, radius, (0,0,255),2)
+            cv2.imshow(title, image)
+            pt = (-1. -1)
             
 
 image = np.full((300, 500, 3), (255, 255, 255), np.uint8) # 흰색 배경 영상
--- SKT_Opencv/prac2_0731.py
+++ SKT_Opencv/prac2_0731.py
@@ -1,31 +1,25 @@
 import cv2
-import numpy as np 
-
-
-def put_string(frame, text, pt, value, color=(120, 200, 90)):             # 문자열 출력 함수 - 그림자 효과
-    text += str(value)
-    shade = (pt[0] + 2, pt[1] + 2)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, text, shade, font, 0.7, (0, 0, 0), 2)  # 그림자 효과
-    cv2.putText(frame, text, pt, font, 0.7, (120, 200, 90), 2)  # 글자 적기
 
 capture = cv2.VideoCapture(0)  # 0번 카메라 연결
 if capture.isOpened() == False:
     raise Exception("카메라 연결 안됨")
 
-# 카메라 속성 획득 및 출력
-print("너비 %d" % capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-print("높이 %d" % capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print("노출 %d" % capture.get(cv2.CAP_PROP_EXPOSURE))
-print("밝기 %d" % capture.get(cv2.CAP_PROP_BRIGHTNESS))
+fps = 30
+delay = round(1000/fps)
+size = (640, 480)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 
 while True:  # 무한 반복
-    ret, frame = capture.read()
+    ret, frame = capture.read()     # 카메라 영상 받기
     if not ret: break
-    if cv2.waitKey(30) >=0: break
+    x, y, w, h = (200,100,100,200)
+    cv2.rectangle(frame, (x,y,w,h), (0,0,255), 3)
 
-    exposure = capture.get(cv2.CAP_PROP_EXPOSURE)
-    put_string(frame, 'EXPOS: ', (10, 40), exposure)
-    title = "View Frame from Camera"
-    cv2.imshow(title, frame)
+    blue, green, red = cv2.split(frame)
+    tmp = green[y:y+h, x:x+w]
+    cv2.add(tmp, 50, tmp)
+    frame = cv2.merge([blue,green, red])
+
+    if cv2.waitKey(delay) >=0: break
+    cv2.imshow("ex16", frame)    # 윈도우에 영상 띄우기
 capture.release()
